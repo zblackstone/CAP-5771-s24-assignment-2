446bee7b5d9c75cbc238d8a4690f8b20f4aefc2d
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-04-01 21:51:09 -0400

    Update part1.py

--- part1.py
+++ part1.py
@@ -53,12 +53,13 @@ def compute():
 
     # Dictionary of 5 datasets. e.g., dct["nc"] = [data, labels]
     # 'nc', 'nm', 'bvv', 'add', 'b'. keys: 'nc', 'nm', 'bvv', 'add', 'b' (abbreviated datasets)
-    
-    noisy_circles = datasets.make_circles(n_samples=100, factor=.5, noise=.05, random_state=42)
-    noisy_moons = datasets.make_moons(n_samples=100, noise=.05, random_state=42)
-    blobs = datasets.make_blobs(n_samples=100, random_state=42)
-    varied = datasets.make_blobs(n_samples=100, cluster_std=[1.0, 2.5, 0.5], random_state=42)
-    
+    random_state = 42
+
+    n_samples = 100
+    noisy_circles = make_circles(n_samples=n_samples, factor=.5, noise=.05, random_state=random_state)
+    noisy_moons = make_moons(n_samples=n_samples, noise=.05, random_state=random_state)
+    blobs = make_blobs(n_samples=n_samples, random_state=random_state)
+    varied = make_blobs(n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=random_state)
     # Generate Anisotropicly distributed data
     transformation = [[0.6, -0.6], [-0.4, 0.8]]
     X_aniso = np.dot(blobs[0], transformation)
